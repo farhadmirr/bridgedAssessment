7e70e8c65f958a1ad0cfcb8725a5c848656fb6da
Farhad <seyedfarhadmir@gmail.com>
2025-01-29 19:47:26 +0330

    domain form added

--- bridgedAssessment/src/components/drawer/drawerComponent.jsx
+++ bridgedAssessment/src/components/drawer/drawerComponent.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import "./drawerComponent.css"
-import { Drawer } from 'antd'
+import { Drawer ,Button,Typography} from 'antd'
+import DomainForm from '../domainForm/domainForm'
+import { CloseOutlined } from '@ant-design/icons'
 const drawerComponent = ({ open, setOpen }) => {
   const onClose = () => {
     setOpen(false)
   }
   return (
-    <Drawer title="Basic Drawer" onClose={onClose} open={open}>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+    <Drawer
+      open={open}
+      onClose={onClose}
+      placement="right"
+      width={'45%'}
+      closeIcon={null}
+      title={
+        <div className="flex justify-between items-center absolute top-[50px]" style={{right: `calc(100% + 50px)`, transform: "rotateY(10deg)"}}>
+          <Button
+            type="text"
+            icon={<CloseOutlined style={{ fontSize: "25px",color:'white' }} />}
+            onClick={onClose}
+          />
+        </div>
+      }
+    >
+
+      <DomainForm onCancel={onClose} />
     </Drawer>
   )
 }
