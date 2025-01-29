786b7554c363978e5deac30eff1c2f5448c84044
farhad-m <farhad.m@caspiands.com>
2025-01-29 12:05:54 +0330

    redux provider added

--- bridgedAssessment/src/main.jsx
+++ bridgedAssessment/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css'
 import MainPage from './pages/mainPage.jsx'
 
 createRoot(
   document.getElementById('root')).render(
-  <StrictMode>
-    <MainPage />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <StrictMode>
+        <MainPage />
+      </StrictMode>,
+    </Provider>
+  )
