87db08b9b00c172e2dab39cac984fb00a6f38902
Farhad <seyedfarhadmir@gmail.com>
2025-01-29 18:01:54 +0330

    status item added

--- bridgedAssessment/src/components/statusItem/statusItem.jsx
+++ bridgedAssessment/src/components/statusItem/statusItem.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
 import { MoreOutlined } from '@ant-design/icons'
+import "./statusItem.css"
+import { Dropdown } from 'antd';
 const statusItem = ({ status }) => {
     const color = status === 'verified'
         ? 'green'
         : status === 'pending'
             ? '#d4d400'
             : 'red';
+
+    const items = [
+        {
+            label: (
+                <span >
+                    View Props
+                </span>
+            ),
+            key: '0',
+            disabled: true,
+        },
+        {
+            label: (
+                <span style={{fontFamily:'Raleway',fontWeight:'700'}}>
+                    Verify
+                </span>
+            ),
+            key: '1',
+        },
+        {
+            label: (
+                <span >
+                   Install Script
+                </span>
+            ),
+            disabled: true,
+            key: '3',
+        },
+        {
+            label: (
+                <span className='text-red-600' style={{fontFamily:'Raleway',fontWeight:'700'}}>
+                    Delete
+                </span>
+            ),
+            key: '4',
+        },
+    ];
     return (
         <div className='w-full flex justify-between'>
             <span style={{ textTransform: 'capitalize', color: color }}>{status.toLowerCase()}</span>
-            <MoreOutlined />
+            <Dropdown menu={{ items }} trigger={['click']}>
+                <MoreOutlined style={{ fontSize: '24px' }} className='cursor-pointer' />
+            </Dropdown>
         </div>
     )
 }
