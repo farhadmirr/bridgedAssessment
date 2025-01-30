84fa32328a264bd1922e97de7364471396746700
farhad-m <farhad.m@caspiands.com>
2025-01-30 12:02:20 +0330

    domain List responsive

--- bridgedAssessment/src/components/domainList/domainList.jsx
+++ bridgedAssessment/src/components/domainList/domainList.jsx
@@ -13,9 +13,9 @@ const domainList = () => {
     <div className='w-full h-[100vh] p-1 md:p-4 lg:p-10 2xl:p-8 3xl:p-14 flex'>
       <div className='w-full flex flex-col gap-3'>
         <h2>Domains</h2>
-        <div className='flex w-full justify-between'>
-          <Button type="primary" icon={<PlusOutlined />} size='large' className='w-56 3xl:w-64' onClick={() => { setDrawer(true) }}>Add Domain</Button>
-          <div className='w-[700px] flex justify-between'>
+        <div className='flex flex-col md:flex-row w-full justify-between'>
+          <Button type="primary" icon={<PlusOutlined />} size='large' className='w-full mb-3 md:mb-0 md:w-56 3xl:w-64' onClick={() => { setDrawer(true) }}>Add Domain</Button>
+          <div className='w-full md:w-[60%] flex flex-col gap-3 md:flex-row justify-end'>
             <SelectComponent id={'orderSelect'} setState={setOrderState} state={orderState} />
             <Input prefix={<SearchOutlined />} placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)} />
           </div>
